01378ac44ec00cf48d14bf08d984ac5150833fd2
Mourchidi <mfoumbymourchidi@gmail.com>
2025-02-04 18:24:30 +0100

    feat:AGED-93: Set jdk version to 17

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
@@ -36,7 +36,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
